8a102c022938b77bd29ad31f2fbf86218662eb80
Sergio <serchazpar@gmail.com>
2023-11-13 20:20:21 +0100

    Subido archivo js funcionalidad

---
+++ funcionalidad.js
@@ -0,0 +1,14 @@
+
+  var tunombre=prompt("Indica tu nombre" ,"Sergio");
+  var tuprimerapellido=prompt("Tu primer apellido" ,"Azparren");
+  var tusegundoapellido=prompt("Tu segundo apellido" ,"Arricibita");
+  var tunacimiento=prompt("Fecha de nacimiento" ,"24/12/1991");
+  var tulugardenacimiento=prompt("Lugar de nacimiento" ,"Pamplona");
+   var saludo="Estos son tus datos\n"+tunombre+"  "+tuprimerapellido+" "+tusegundoapellido+"\nNacido/a el: " +tunacimiento+"\nEn: "+ tulugardenacimiento;
+   alert(saludo);
+  
+  
+  const d = new Date(2023,8,14);
+  alert(d);
+   
+  
